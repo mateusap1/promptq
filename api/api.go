37590e0c5cb8208d36ab11b2b08509c22a1a302b
Mateus Santos <santos.mateus@protonmail.com>
2024-07-09 15:02:25 -0300

    created api data types

--- api/api.go
+++ api/api.go
@@ -3,3 +3,17 @@ package api
 type HealthResponse struct {
 	Message string
 }
+
+type RequestPromptResponse struct {
+	QueueId string
+}
+
+type PromptResponse struct {
+	Prompt   string
+	State    string
+	Response string
+}
+
+type PeekPromptResponse = PromptResponse
+type DeQueuePromptResponse = PromptResponse
+type RespondPromptResponse = PromptResponse
