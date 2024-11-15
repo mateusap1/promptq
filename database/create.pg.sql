6eac19fd5a31c5fa24d5a535810973c5ef438d54
Mateus Santos <santos.mateus@protonmail.com>
2024-11-15 06:59:21 -0300

    sessions schema

--- database/create.pg.sql
+++ database/create.pg.sql
@@ -8,5 +8,16 @@ CREATE TABLE users (
     reset_token VARCHAR NULL,
     reset_token_expires TIMESTAMP NULL,
     created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
+    updated_at TIMESTAMP
+);
+
+CREATE TABLE sessions (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users ON DELETE CASCADE,
+    user_agent VARCHAR NOT NULL,
+    ip_address VARCHAR NOT NULL,
+    session_token VARCHAR NOT NULL,
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT NOW(),
+    expires_at TIMESTAMP
 );
